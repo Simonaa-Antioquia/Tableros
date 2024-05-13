b31788ec7f24b032529b4c8ddf98f3471299e08c
Lau4159 <lmquinterv@eafit.edu.co>
2024-05-13 13:01:47 -0500

    Tablero3_f

---
+++ Abs3/003e_Indices_recibe_de_antioquia_cadena_funcion.R
@@ -0,0 +1,148 @@
+#Proyecto FAO
+#Procesamiento datos SIPSA
+################################################################################-
+#Autores: Juliana Lalinde, Laura Quintero, Germán Angulo
+#Fecha de creacion: 02/04/2024
+#Fecha de ultima modificacion: 02/04/2024
+################################################################################-
+# Limpiar el entorno de trabajo
+rm(list=ls())
+# Paquetes 
+################################################################################-
+library(readr);library(lubridate);library(dplyr);library(ggplot2);library(zoo);library(readxl)
+library(glue);library(tidyverse);library(gridExtra);library(corrplot);library(ggrepel);library(leaflet)
+library(sf);library(plotly)
+options(scipen = 999)
+################################################################################-
+
+# Cargar el shapefile
+shapefile <- st_read("MGN_DPTO_POLITICO.shp")%>%
+  janitor::clean_names()%>%filter(dpto_ccdgo!=88)%>%mutate(dpto_ccdgo=as.numeric(dpto_ccdgo))
+
+antioquia<-read.csv("base_indices_cadena_antioquia.csv")
+#antioquia$depto_origen[antioquia$depto_origen=="BOGOTÁ, D. C."]<-"BOGOTÁ, D.C."
+
+col_en_ant<-function(Año = NULL, Mes = NULL, Producto = NULL){
+  
+  df<-antioquia
+  
+  # Año seleccionado
+  if (!is.null(Año)) {
+    df <- df %>% dplyr::filter(anio == Año)
+  }
+  
+  # Mes seleccionado, si se proporciona
+  if (!is.null(Mes)) {
+    df <- df %>% dplyr::filter(mes == Mes)
+  }
+  
+  # Si se especifica un producto
+  if (!is.null(Producto)) {
+    df <- df %>% dplyr::filter(producto == Producto)
+  }
+  
+  if (!is.null(Año) && !is.null(Mes) && !is.null(Producto)) {
+    
+    df<- df  %>%
+      distinct(anio,mpio_destino,mpio_destino,codigo_depto_destino,producto,mes, .keep_all = TRUE) %>%
+      select(anio,mpio_destino,codigo_depto_destino, mes, producto, porcentaje_dpto_mes_producto)  
+    df <- df %>% rename( columna_porcentaje = porcentaje_dpto_mes_producto)
+    
+  } else if (!is.null(Año) && !is.null(Mes)) {
+    
+    df<- df  %>%
+      distinct(anio,mpio_destino,codigo_depto_destino,mes, .keep_all = TRUE) %>%
+      select(anio,mpio_destino,codigo_depto_destino, mes, porcentaje_dpto_mes)  
+    columna_porcentaje <- "porcentaje_dpto_mes"
+    df <- df %>% rename( columna_porcentaje = porcentaje_dpto_mes)
+    
+    
+  } else if (!is.null(Año) && !is.null(Producto)) {
+    
+    df<- df  %>%
+      distinct(anio,mpio_destino,codigo_depto_destino,producto, .keep_all = TRUE) %>%
+      select(anio,mpio_destino,codigo_depto_destino, producto, porcentaje_dpto_anio_producto)  
+    columna_porcentaje <- "porcentaje_dpto_anio_producto"
+    df <- df %>% rename( columna_porcentaje =porcentaje_dpto_anio_producto)
+    
+  } else if (!is.null(Año)){
+    # No se tienen ni mes ni producto
+    
+    df<- df  %>%
+      distinct(anio,mpio_destino,codigo_depto_destino, .keep_all = TRUE) %>%
+      select(anio,mpio_destino,codigo_depto_destino, porcentaje_dpto_anio)  
+    df <- df %>% rename( columna_porcentaje = porcentaje_dpto_anio)
+    
+  }else if (!is.null(Producto)){
+    # No se tienen ni mes ni producto
+    
+    df<- df  %>%
+      distinct(mpio_destino,codigo_depto_destino,producto, .keep_all = TRUE) %>%
+      select(mpio_destino,codigo_depto_destino,producto, porcentaje_dpto_prod)  
+    df <- df %>% rename( columna_porcentaje = porcentaje_dpto_prod)
+    
+  } else {
+    
+    df<- df  %>%
+      distinct(mpio_destino,codigo_depto_destino, .keep_all = TRUE) %>%
+      select(mpio_destino,codigo_depto_destino, porcentaje_dpto)  
+    df <- df %>% rename( columna_porcentaje = porcentaje_dpto)
+    
+  }
+  df<-df#%>%filter(codigo_depto_destino !=5)
+  if (nrow(df) == 0) {
+    print("No hay datos disponibles")
+  } else {mapa<-shapefile%>%left_join(df, by = c("dpto_ccdgo"="codigo_depto_destino"))%>%
+    mutate(columna_porcentaje=columna_porcentaje*100)
+  
+  # Crear un título dinámico
+  #titulo <- paste("Porcentaje que llega de Antioquia del total", ifelse(is.null(Año), "", paste("en el año", Año)),
+  #                ifelse(is.null(Mes), "", paste("para el mes", Mes)), 
+  #                ifelse(is.null(Producto), "", paste("-", Producto)))
+  
+  mapa <- mapa %>% #filter(!is.na(columna_porcentaje))%>% 
+    arrange(columna_porcentaje)%>%mutate(columna_porcentaje2=columna_porcentaje*-1)
+  
+  min_val <- min(abs(na.omit(mapa$columna_porcentaje)))
+  max_val <- max(abs(na.omit(mapa$columna_porcentaje)))
+  
+  valores_sin_na <- na.omit(mapa$columna_porcentaje)
+  my_palette_sin_na <- colorNumeric(palette = c("#F2E203", "#1A4922"), domain = valores_sin_na)
+  
+  # Crear una paleta de colores personalizada
+  my_palette <- colorNumeric(palette = colorRampPalette(c("#F2E203", "#1A4922"))(length(unique(mapa$columna_porcentaje))), domain = c(min_val,max_val),  na.color = "#C5C7C6")
+  
+  # Crear el mapa interactivo
+  p <- leaflet(mapa) %>%
+    addProviderTiles(providers$CartoDB.Positron) %>%
+    addPolygons(fillColor = ~my_palette(columna_porcentaje),
+                fillOpacity = 0.8, 
+                color = "#D5D5D5", 
+                weight = 1,
+                popup = ~ifelse(is.na(columna_porcentaje),"",paste0("<strong>Departamento: </strong>", tools::toTitleCase(tolower(mapa$dpto_cnmbr)), 
+                                "<br><strong>Diferencia del precio: </strong>", round(columna_porcentaje),"%")),
+                highlightOptions = highlightOptions(color = "white", 
+                                                    weight = 2,
+                                                    bringToFront = FALSE)) %>%
+    addLegend(pal = my_palette_sin_na, values = ~valores_sin_na, opacity = 0.7, title = "Porcentaje")#, na.label = "")
+    
+      porcentaje_max<-round(max(df$columna_porcentaje)*100,1)
+      # Encontrar el índice del valor máximo ignorando los NA
+      indice_max <- which(mapa$columna_porcentaje == max(mapa$columna_porcentaje, na.rm = TRUE))
+      # Convertir a formato de título
+      dpto_max <- tools::toTitleCase(tolower(mapa$dpto_cnmbr[indice_max]))
+      return(list(
+        grafico=p,
+        datos=df,
+        porcentaje_max=porcentaje_max,
+        dpto_max=dpto_max
+      ))
+  }
+}
+
+#col_en_ant()
+#col_en_ant(Año=2023)
+#col_en_ant(Año=2023, Mes=1)
+#col_en_ant(Año=2013, Mes=1, Producto = "tomates otros")
+#col_en_ant(Año=2023, Producto = "arroz")
+#col_en_ant(Producto = "arroz")
---
+++ Abs3/server.R
@@ -0,0 +1,115 @@
+
+
+# Definir la función de servidor
+server <- function(input, output, session) {
+  
+  resultado<-reactive({
+    # Comprobar si solo se ha seleccionado un producto
+    if (input$producto != "todo" && input$anio == "todo" && input$mes == "todo") {
+      col_en_ant(Producto = input$producto)
+    } else if (input$mes != "todo" && input$anio == "todo") {
+      validate(
+        need(input$anio != "todo", "Debe seleccionar un año.")
+      )
+    } else if(input$anio == "todo" && input$producto == "todo" && input$mes == "todo"){
+      col_en_ant()
+    } else if(input$producto == "todo" && input$mes == "todo" ){
+      col_en_ant(Año = input$anio)
+    } else if(input$producto == "todo"){
+      col_en_ant(Año = input$anio, Mes = input$mes)
+    } else if(input$mes == "todo" ){
+      col_en_ant(Año = input$anio, Producto = input$producto)
+    } else if(input$anio == "todo" && input$mes == "todo"){
+      col_en_ant(Producto = input$producto)
+    } else{
+      col_en_ant(Año = input$anio, Mes = input$mes ,Producto = input$producto)
+    }  
+  })
+  
+  observeEvent(input$reset, {
+    updateSelectInput(session, "producto", selected = "todo")
+    updateSelectInput(session, "mes", selected = "todo")
+    updateSelectInput(session, "anio", selected = "todo")
+  })
+  
+  output$grafico <- renderLeaflet({
+    res <- resultado()
+    if (is.data.frame(res) || is.list(res)) {
+      if (nrow(res$datos) == 0) {
+        print("No hay datos disponibles")
+        leaflet()  # Devuelve un mapa de Leaflet vacío
+      } else {
+        res$grafico %>%
+          setView(lng = -75.5, lat = 3.9, zoom = 5) 
+      }
+    } else {
+      print("No hay datos disponibles")
+      leaflet()  # Devuelve un mapa de Leaflet vacío
+    }
+  })
+  
+  output$vistaTabla <- renderTable({
+    if (!is.null(resultado()$datos)) {
+      head(resultado()$datos, 5)
+    }
+  })
+  
+  output$descargar <- downloadHandler(
+    filename = function() {
+      paste("grafica-", Sys.Date(), ".png", sep="")
+    },
+    content = function(file) {
+      tempFile <- tempfile(fileext = ".html")
+      htmlwidgets::saveWidget(plotly::as_widget(resultado()$grafico), tempFile, selfcontained = FALSE)
+      webshot::webshot(tempFile, file = file, delay = 2, vwidth = 800, vheight = 800)
+    }
+  )
+  
+  output$descargarDatos <- downloadHandler(
+    filename = function() {
+      paste("datos-", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(resultado()$datos, file)
+    }
+  )
+  
+  observeEvent(input$github, {
+    browseURL("https://github.com/PlasaColombia-Antioquia/Tableros.git")
+  })
+  
+  output$subtitulo <- renderText({
+    res <- resultado()
+    if (is.data.frame(res) || is.list(res)) {
+      if(nrow(res$datos) == 0) {
+        return("No hay datos disponibles")
+      }else{
+        porcentaje_max <- res$porcentaje_max
+        dpto_max <- res$dpto_max
+        
+        return(paste0(dpto_max," recibe el ", porcentaje_max, "% de lo que ingresa de Antioquia, siendo el departamento que más recibe de este."))
+      }
+    } else {
+      return("No hay datos disponibles")
+    }
+  })
+  
+  output$mensaje1 <- renderText({
+    #resultado <- resultado()
+    #volatil<-resultado$producto_vol
+    return("Poner mensaje")
+  })
+  
+  output$mensaje2 <- renderText({
+    #resultado <- resultado()
+    #promedio_camb<-resultado$promedio_camb
+    return("Poner mensaje")
+  })
+  
+  output$mensaje3 <- renderText({
+    #resultado <- resultado()
+    #promedio_camb_an<-resultado$promedio_camb_an
+    return("Poner mensaje")
+  })
+}
+
