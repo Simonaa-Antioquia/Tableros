04fb8b4e215891e37e0aac71768a1d17a5667d03
Juliana Lalinde Velásquez <41337746+julianalalindev@users.noreply.github.com>
2024-05-14 20:08:31 -0500

    Update server.R

--- Maps2/server.R
+++ Maps2/server.R
@@ -6,26 +6,37 @@ server <- function(input, output, session) {
   #})
   
   resultado<-reactive({
+    #Comprobar si se selecciono como minimo una ruta para apagar
+    if(is.null(input$ruta)) {
       # Comprobar si solo se ha seleccionado un producto
-      if (input$producto != "" && input$anio == "" && input$mes == "" && input$ruta == "") {
+      if (input$producto != "" && input$anio == "" && input$mes == "") {
         ruta(Producto = input$producto)
       } else if (input$mes != "" && input$anio == "") {
         validate(
           need(input$anio != "", "Debe seleccionar un año.")
         )
-      } else if(input$anio == "" && input$producto == "" && input$mes == "" && input$ruta == ""){
+      } else if(input$anio == "" && input$producto == "" && input$mes == ""){
         ruta()
       } else if(input$producto == "" && input$mes == "" ){
-        ruta(Año = input$anio, Rutas = input$ruta)
+        ruta(Año = input$anio)
       } else if(input$producto == ""){
-        ruta(Año = input$anio, Mes = input$mes, Rutas = input$ruta)
+        ruta(Año = input$anio, Mes = input$mes)
       } else if(input$mes == "" ){
-        ruta(Año = input$anio, Producto = input$producto, Rutas = input$ruta)
+        ruta(Año = input$anio, Producto = input$producto)
       } else if(input$anio == "" && input$mes == ""){
-        ruta(Producto = input$producto, Rutas = input$ruta)
+        ruta(Producto = input$producto)
       } else{
-        ruta(Año = input$anio, Mes = input$mes,Producto = input$producto, Rutas = input$ruta)
+        ruta(Año = input$anio, Mes = input$mes,Producto = input$producto)
       }
+    } else {
+        r1 <- "r1" %in% input$ruta
+        r2 <- "r2" %in% input$ruta
+        r3 <- "r3" %in% input$ruta
+        r4 <- "r4" %in% input$ruta
+        r5 <- "r5" %in% input$ruta
+
+        rutas <- c
+    }
   })
   
   output$plot <- renderLeaflet({
