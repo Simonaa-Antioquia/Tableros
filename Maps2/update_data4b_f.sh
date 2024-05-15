dd1ecb24ba8f26939d107b06eecc5c7d2014b188
Juliana Lalinde Velásquez <41337746+julianalalindev@users.noreply.github.com>
2024-05-15 08:52:53 -0500

    Update update_data4b_f.sh

--- Maps2/update_data4b_f.sh
+++ Maps2/update_data4b_f.sh
@@ -1,16 +1,16 @@
 ## Change Permission in folder
-sudo rm -rf  /srv/shiny-server/Tablero4
-sudo mkdir /srv/shiny-server/Tablero4
+sudo rm -rf  /srv/shiny-server/Maps2
+sudo mkdir /srv/shiny-server/Maps2
 
 
 ## Change Permission in folder
-sudo chown -R shiny:shiny /srv/shiny-server/Tablero4
+sudo chown -R shiny:shiny /srv/shiny-server/Maps2
 
 ### Update files
-sudo rsync -a --recursive  /home/rstudio/Tableros/Tablero4_b/ /srv/shiny-server/Tablero4_b --delete
+sudo rsync -a --recursive  /home/rstudio/Tableros/Maps2/ /srv/shiny-server/Maps2 --delete
 
 ## Change Permission in folder
-sudo chown -R shiny:shiny /srv/shiny-server/Tablero4
+sudo chown -R shiny:shiny /srv/shiny-server/Maps2
 
 ### refresh shiny-server
 sudo systemctl stop shiny-server
