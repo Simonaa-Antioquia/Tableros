0adfc5e6a71a79b952a632c473c5599fa34cc59d
Juliana Lalinde Velásquez <41337746+julianalalindev@users.noreply.github.com>
2024-06-05 08:04:47 -0500

    Update ui.R

--- Maps1/ui.R
+++ Maps1/ui.R
@@ -43,6 +43,11 @@ ui <- fluidPage(
         font-size: 40px;
         color: #0D8D38;
       }
+    .main-header_2 {
+        font-family: 'Prompt', sans-serif;
+        font-size: 20px;
+        color: #0D8D38;
+      }
       .sub-header {
         font-family: 'Prompt', sans-serif;
         font-size: 20px;
@@ -51,9 +56,28 @@ ui <- fluidPage(
         font-family: 'Prompt', sans-serif;
         font-size: 15px;
       }
+.sub-header3 {
+        font-family: 'Prompt', sans-serif;
+        font-size: 15px;
+      }
+      .center {
+        display: flex;
+        justify-content: center;
+      }
+      .scrollable-content {
+        overflow-y: auto;
+        overflow-x: hidden;
+        height: auto;
+      }
     "))
   ),
-  tags$h1("¿Qué rutas toman los alimentos que llegan a Antioquia?", class = "main-header"),
+  tags$h1("Rutas de los alimentos que ingresan a Medellín", class = "main-header"),
+  tags$h1("Visualiza las rutas que transitan los alimentos que ingresan a Medellín", class = "main-header_2"),
+    div(
+    textOutput("subtitulo"),
+    class = "sub-header2",
+    style = "margin-bottom: 20px;"
+  ),  
   div(
       fluidRow(
         column(2,
@@ -64,10 +88,10 @@ ui <- fluidPage(
                selectInput("producto", "Producto",c("Todos los productos" = "", sort(as.character(unique(abastecimiento_medellin$producto)))))),
         column(4,
                selectInput("importancia","Importancia de los municipios",c("No incluir" = "","Incluir")))
-      )),
-    div(fluidRow(
+      ),
+    fluidRow(
       column(7,  
-             leafletOutput("plot", width = "100%", height = "250px"),
+             leafletOutput("plot"),
              actionButton("descargar", "Gráfica", icon = icon("download")),
              downloadButton("descargarDatos", "Datos"),
              #actionButton("github", "GitHub", icon = icon("github")),
@@ -87,14 +111,19 @@ ui <- fluidPage(
         ))
     ),
            tags$div(
-             tags$p("Este mapa permite identificar las rutas de donde vienen los alimentos que abastecen la central mayorista y minorista de Medellín, permitiendo identificar la importancia de cada una para el abastecimiento total.", class = "sub-header2", style = "margin-top: 3px;"),
              tags$p("Fuente: Cálculos propios a partir de datos del Sistema de Información de Precios y Abastecimiento del Sector Agropecuario (SIPSA)", class = "sub-header2", style = "margin-top: 3px;")
+             tags$p("Este mapa permite identificar las rutas de donde vienen los alimentos que abastecen la central mayorista y minorista de Medellín, permitiendo identificar la importancia de cada una para el abastecimiento total.", class = "sub-header2", style = "margin-top: 3px;"
+             )
            
     )
   ),
+   br(),
+  br(),
+  br(),
+  br(),
   tags$div(
-    tags$img(src = 'logo.jpeg', style = "width: 100%; margin: 0;"),  
-    style = "position: fixed; bottom: 0; width: 100%; margin:0;"  
+    tags$img(src = 'logo_2.png', style = "width: 100vw;"),
+    style = "position: absolute; bottom: 0; width: 100%;"
   )
   )
 )
