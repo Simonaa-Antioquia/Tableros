5ddc3826dd656224a11e0aa2af04297a155efe26
Juliana Lalinde Velásquez <41337746+julianalalindev@users.noreply.github.com>
2024-06-05 22:57:37 -0500

    Update ui.R

--- Maps2/ui.R
+++ Maps2/ui.R
@@ -96,12 +96,12 @@ ui <- fluidPage(
     fluidRow(
       column(7,  
              leafletOutput("plot"),
-             actionButton("descargar", "Gráfica", icon = icon("download")),
+             #actionButton("descargar", "Gráfica", icon = icon("download")),
              downloadButton("descargarDatos", "Datos"),
              #actionButton("github", "GitHub", icon = icon("github")),
              shiny::a("GitHub", href="https://github.com/PlasaColombia-Antioquia/Tableros.git", target="_blank",
                       class = "btn btn-default shiny-action-button", icon("github")),
-             actionButton("go", "Reporte", icon = icon("file-alt")),
+             #actionButton("go", "Reporte", icon = icon("file-alt")),
              actionButton("reset", "Restrablecer",icon = icon("refresh"))
       ),
       column(5, 
