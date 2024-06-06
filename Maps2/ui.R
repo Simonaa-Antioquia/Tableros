befc172936cf9d3378e5f7398572e96d05ef8fe3
Juliana Lalinde Velásquez <41337746+julianalalindev@users.noreply.github.com>
2024-06-05 21:45:17 -0500

    Update ui.R

--- Maps2/ui.R
+++ Maps2/ui.R
@@ -71,12 +71,12 @@ ui <- fluidPage(
   div(
       fluidRow(
         column(2,
-               selectInput("anio", "Año", c("Todos los años" = "", sort(as.character(unique(abastecimiento_medellin$anio)))))),
+               selectInput("anio", "Año", c("Todos los años" = "todo", sort(as.character(unique(abastecimiento_medellin$anio)))))),
+        column(3,
+               selectInput("mes", "Mes", c("Todos los meses" = "todo", "Enero" = 1, "Febrero" = 2, "Marzo" = 3, "Abril" = 4, "Mayo" = 5, "Junio" = 6, "Julio" = 7, "Agosto" = 8, "Septiembre" = 9, "Octubre" = 10, "Noviembre" = 11, "Diciembre" = 12), selected="")),
         column(2,
-               selectInput("mes", "Mes", c("Todos los meses" = "", sort(as.numeric(unique(abastecimiento_medellin$mes)))))),
-        column(2,
-               selectInput("producto", "Producto",c("Todos los productos" = "", sort(as.character(unique(abastecimiento_medellin$producto)))))),
-        column(6,
+               selectInput("producto", "Producto",c("Todos los productos" = "todo", sort(as.character(unique(abastecimiento_medellin$producto)))))),
+        column(5,
                 checkboxGroupInput("ruta", "Rutas a cerrar:",
                                  c("Antioquia" = 99,
                                    "Norte" = 1,
